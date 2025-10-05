9c1dbd6d1d09ec40df1cf2759c641ddb79325bfc
AdamKuzniarski <adam.kuzniarski@gmx.de>
2025-10-05 12:50:12 +0200

    add start game function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,20 @@
 const STATE = {
   canHit: true,
-  running: false,
+  isRunning: false,
   score: 0,
   timeLeft: 30,
   activeIndex: null,
   lastIndex: null,
+  loopTimeout: null,
+  hideTimeout: null,
+  timerInterval: null,
 };
 
+function randomGenerator() {
+  return Math.floor(Math.random() * 9);
+}
+
+console.log(randomGenerator());
 const gameContainer = document.getElementById("game-container");
 const boardEl = document.getElementById("board");
 const holes = document.querySelectorAll(".hole");
@@ -14,3 +22,15 @@ const scoreValue = document.getElementById("scoreValue");
 const timeValue = document.getElementById("timeValue");
 const startButton = document.getElementById("start-button");
 const stopButton = document.getElementById("stop-button");
+
+function startGame() {
+  if (STATE.isRunning) {
+    STATE.score = 0;
+    STATE.timeLeft = 30;
+    activeIndex = null;
+    lastIndex = null;
+    canHit = false;
+  }
+  console.log("Game is already running");
+}
+startGame();
