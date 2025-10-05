a66aa53eb9f33656717692f26fc70dbc6417e257
AdamKuzniarski <adam.kuzniarski@gmx.de>
2025-10-05 14:21:15 +0200

    add tick() and hide()

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,11 +10,6 @@ const STATE = {
   timerInterval: null,
 };
 
-function randomGenerator() {
-  return Math.floor(Math.random() * 9);
-}
-
-console.log(randomGenerator());
 const gameContainer = document.getElementById("game-container");
 const boardEl = document.getElementById("board");
 const holes = document.querySelectorAll(".hole");
@@ -23,14 +18,57 @@ const timeValue = document.getElementById("timeValue");
 const startButton = document.getElementById("start-button");
 const stopButton = document.getElementById("stop-button");
 
+function getRandomIndex(excludeIndex) {
+  if (excludeIndex === undefined || excludeIndex === null) {
+    return Math.floor(Math.random() * 9);
+  }
+  let newIndex;
+  do {
+    newIndex = Math.floor(Math.random() * 9);
+  } while (newIndex === excludeIndex);
+  return newIndex;
+}
+
 function startGame() {
   if (STATE.isRunning) {
+    return console.log("Game is already running");
+  } else {
+    STATE.isRunning = true;
     STATE.score = 0;
     STATE.timeLeft = 30;
-    activeIndex = null;
-    lastIndex = null;
-    canHit = false;
+    STATE.activeIndex = null;
+    STATE.lastIndex = null;
+    STATE.canHit = false;
+    scoreValue.textContent = STATE.score;
+    timeValue.textContent = STATE.timeLeft;
+    startButton.disabled = true;
+    stopButton.disabled = false;
+    gameContainer.classList.add("active");
   }
-  console.log("Game is already running");
 }
-startGame();
+startButton.addEventListener("click", startGame);
+
+function hide() {
+  if (!STATE.isRunning) return;
+}
+
+function tick() {
+  if (!STATE.isRunning) return;
+  if (STATE.isRunning) {
+    const holesElement = holes[newIndex];
+    holesElement = getRandomIndex(STATE.lastIndex);
+    STATE.activeIndex !== null
+      ? holes[STATE.activeIndex].classList.remove("active")
+      : null;
+    holesElement.classList.add("active");
+    STATE.activeIndex = holesElement;
+    STATE.lastIndex = holesElement;
+    STATE.canHit = true;
+    STATE.hideTimeout = setTimeout(hide, 850);
+  }
+}
+
+function scheduleNextTick() {
+  if (!STATE.isRunning) return;
+  const nextTickIn = Math.floor(Math.random() * 1000) + 400;
+}
